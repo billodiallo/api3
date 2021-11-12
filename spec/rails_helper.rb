d141a3b70965c7dbb5e7374e2d241d820428e44c
Billo Diallo <billodiallonet@gmail.com>
2021-11-12 14:34:45 +0300

    Add test GET categories method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -31,7 +31,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.include RequestSpecHelper, type: :request
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -66,7 +65,6 @@ Shoulda::Matchers.configure do |config|
  end
 end
 # ...
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 RSpec.configure do |config|
   # ...
   # add `FactoryBot` methods
@@ -83,6 +81,7 @@ RSpec.configure do |config|
     end
   end
   # ...
+  config.include RequestSpecHelper, type: :request
 
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
@@ -93,6 +92,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-
 
 end
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+# initialize test data
+let!(:categories) { create_list(:category, 5) }
+let!(:category_id) { categories.first.id }
+    
   end
-end
+  describe 'GET /categories' do
+    # make HTTP get request before each example
+    before { get '/api/v1/categories' }
+    it 'returns categories' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(5)
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
